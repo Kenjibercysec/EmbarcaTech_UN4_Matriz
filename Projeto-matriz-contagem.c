5ecf9c0887bd698fdaf9054e4fdf72f5d234803d
kenji <silasotsuka@gmail.com>
2025-02-02 16:01:31 -0300

    Comentanto funções

--- Projeto-matriz-contagem.c
+++ Projeto-matriz-contagem.c
@@ -7,12 +7,16 @@
 #include "pico/bootrom.h"
 #include "Projeto-matriz-contagem.pio.h"
 
-#define LED_PIN_RED 13
+// ----------------------
+
 #define NUM_PIXELS 25
+#define LED_PIN_RED 13
 #define LED_PIN 7
+
 #define BOTAO_A 5
 #define BOTAO_B 6
 
+//------------------------
 int static volatile indice = 0;
 uint count = 0;
 uint actual_time = 0;
@@ -20,12 +24,15 @@ uint valor_led;
 uint sm;
 PIO pio = pio0;
 
+//-------------------------
 void piscar_led(){
   gpio_put(LED_PIN_RED, true);
   sleep_ms(25);
   gpio_put(LED_PIN_RED, false);
   sleep_ms(175);
 }
+
+//-------------------------
 
 uint matrix_rgb(float r, float g, float b){
   unsigned char R, G, B;
@@ -35,6 +42,8 @@ uint matrix_rgb(float r, float g, float b){
   return (G << 24) | (R << 16) | (B << 8);
 }
 
+//-------------------------
+
 int getIndex(int x, int y){
   if (y % 2 == 0){
     return 24 - (y * 5 + x);
@@ -43,6 +52,8 @@ int getIndex(int x, int y){
   }
 }
 
+//-------------------------
+
 void new_index(){
   if (indice > 10){
     indice = 0;
@@ -50,6 +61,8 @@ void new_index(){
     indice = 10;
   }
 }
+
+//-------------------------
 
 void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
   for (int16_t i = 0; i < NUM_PIXELS; i++)
@@ -138,6 +151,9 @@ double numero9[25] = {
 
 double *numeros[11] = {apagar_leds, numero0, numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9};
 
+
+//-------------------------
+
 void callback_button(uint gpio, uint32_t events) {
     uint time = to_ms_since_boot(get_absolute_time());
     if (time - actual_time > 250) {
@@ -155,7 +171,9 @@ void callback_button(uint gpio, uint32_t events) {
         }
     }
 }
-        
+
+//-------------------------
+
 int main() {
   bool frequenciaClock;
   uint16_t i;
@@ -185,7 +203,9 @@ int main() {
   uint offset = pio_add_program(pio, &pio_matrix_program);
   sm = pio_claim_unused_sm(pio, true);
   pio_matrix_program_init(pio, sm, offset, LED_PIN);
-    
+  
+  //-------------------------
+  
   desenho_pio(apagar_leds, valor_led, pio, sm, r, g, b);
 
   gpio_set_irq_enabled_with_callback(BOTAO_A, GPIO_IRQ_EDGE_FALL, true, &callback_button);
